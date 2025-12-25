b9b44aab499baef635c3be32aa74c9f880b35a1c
Rupesh010 <130222600+Rupesh010@users.noreply.github.com>
2025-12-25 19:52:08 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("The sum of the numbers is: ", num1 + num2)
+print("The Substraction of the numbers is: ", num1 - num2)
+print("The product of the numbers is: ", num1 * num2)
+print("The Division of the numbers is: ", round(num1 / num2, 2))
